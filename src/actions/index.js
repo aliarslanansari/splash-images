460ef23b18f4f84c43bf5885e1ef559b331262d0
Ali Arsalan Ansari <aliarslanansari@hotmail.com>
2020-08-21 04:57:16 +0530

    added stats saga

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { IMAGES } from '../constants';
+import { STATS, IMAGES } from '../constants';
 const loadImages = () => ({ type: IMAGES.LOAD });
 const setImages = images => ({
     type: IMAGES.LOAD_SUCCESS,
@@ -8,4 +8,26 @@ const setError = error => ({
     type: IMAGES.LOAD_FAIL,
     error,
 });
-export { loadImages, setImages, setError };
+
+const loadImageStats = id => ({
+    type: STATS.LOAD,
+    id,
+});
+const setImageStats = (id, downloads) => ({
+    type: STATS.LOAD_SUCCESS,
+    id,
+    downloads,
+});
+const setImageStatsError = error => ({
+    type: STATS.LOAD_FAIL,
+    error,
+});
+
+export {
+    loadImages,
+    setImages,
+    setError,
+    loadImageStats,
+    setImageStats,
+    setImageStatsError,
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import loadingReducer from './loadingReducer';
 import imagesReducer from './imagesReducer';
 import errorReducer from './errorReducer';
 import pageReducer from './pageReducer';
+import statsReducer from './statsReducer';
 
 import { combineReducers } from 'redux';
 const rootReducer = combineReducers({
@@ -9,6 +10,7 @@ const rootReducer = combineReducers({
     images: imagesReducer,
     error: errorReducer,
     nextPage: pageReducer,
+    imageStats: statsReducer,
 });
 
 export default rootReducer;
